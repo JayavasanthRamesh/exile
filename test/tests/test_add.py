58683175b3497b80cd2f633613615e683d660389
patstam <mepjay@gmail.com>
2014-03-10 19:02:41 -0700

    Adding test for file update

--- test/tests/test_add.py
+++ test/tests/test_add.py
@@ -18,3 +18,18 @@ class AddTest(ExileTest):
         path, contents = self._files.items()[0]
         self.exile('add ' + path)
         self.assertAdded(contents)
+
+    def test_update(self):
+        path = 'updateme'
+        initial = 'initial'
+        updated = 'different'
+
+        # create the file and add it
+        create_file(self._dir, path, initial)
+        self.exile('add ' + path)
+        self.assertAdded(initial)
+
+        # change the contents and add it again (update)
+        create_file(self._dir, path, updated)
+        self.exile('add ' + path)
+        self.assertAdded(updated)
