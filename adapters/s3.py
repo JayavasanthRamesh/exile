373c4f7fdebcd6db6f20d29ba8e6359945bd9e17
patstam <mepjay@gmail.com>
2014-03-11 16:25:54 -0700

    Lazy connection creation for S3 adapter

--- adapters/s3.py
+++ adapters/s3.py
@@ -14,20 +14,30 @@ template = {
 class Communicator:
     def __init__(self, config):
         try:
-            # uses HTTPS by default
-            conn = boto.connect_s3(config['id'], config['secret'])
-            self.__bucket = conn.get_bucket(config['bucket'])
+            # grab the configuration values to catch missing config early
+            self.__id = config['id']
+            self.__secret = config['secret']
+            self.__bucket_name = config['bucket']
+            self.__bucket_conn = None   # connection initialized lazily
         except KeyError as e:
             raise Exception("missing required configuration: " + str(e))
 
         self.__encrypt = config.get('encrypt', False)
 
+    def __bucket(self):
+        """Lazily creates the S3 connection, which is then reused for future requests."""
+        if self.__bucket_conn is None:
+            # uses HTTPS by default
+            conn = boto.connect_s3(self.__id, self.__secret)
+            self.__bucket_conn = conn.get_bucket(self.__bucket_name)
+        return self.__bucket_conn
+
     def get(self, hash, dest):
-        key = self.__bucket.new_key(hash)
+        key = self.__bucket().new_key(hash)
         tmpfd, tmp = tempfile.mkstemp()
         with os.fdopen(tmpfd, 'wb') as file:
             key.get_contents_to_file(file)
         shutil.move(tmp, dest)
 
     def put(self, source, hash):
-        self.__bucket.new_key(hash).set_contents_from_filename(source, encrypt_key=self.__encrypt)
+        self.__bucket().new_key(hash).set_contents_from_filename(source, encrypt_key=self.__encrypt)
